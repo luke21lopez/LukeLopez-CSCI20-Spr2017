1d3f4d20e7c0240b9e8bc42d92ed3aa480338bde
Luke Lopez <llopez020@student.butte.edu>
2017-01-26 22:54:03 +0000

    lab 1.1

---
+++ lab11/lab11.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+// Start program
+int main () {
+// Have computer randomly select number
+ 
+ // Store number as 'a'
+
+// prompt user to choose number from 1 - 10
+
+// store user input as 'b'
+
+// is userinput is less than compnumber, tell user too low
+
+// if userinput is higher than compnumber, tell user too high
+
+// if userinput is equal to compnumber, congradulate user
+
+// continue until correct number has been chosen.
+
+return 0;
+}
