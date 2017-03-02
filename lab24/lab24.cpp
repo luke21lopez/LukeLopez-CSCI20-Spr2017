dbf920a24264acbb84b53780d4e651eb4ccd2820
Luke Lopez <llopez020@student.butte.edu>
2017-03-02 22:25:00 +0000

    lab24 submission

--- lab24/lab24.cpp
+++ lab24/lab24.cpp
@@ -10,14 +10,20 @@
 using namespace std;
 
 
+// This lab will take in two integers inputted by the user.
+// Then it will find a random number between those numbers.
+// it will use that random numbers as pounds and convert it into kilograms
+// then it will covert the kilograms back into pounds
 
-int randomNumber(int, int);
 
-void swapNumber(int&,int&);
 
-double PoundsToKilograms(double);
+int randomNumber(int, int); // Declaring randomNumber
 
-double KilosToPounds(double);
+void swapNumber(int&,int&); // Declaring swapNumber
+
+double PoundsToKilograms(double); // Declaring pounds to kilograms
+
+double KilosToPounds(double); // Declaring kilos to pounds function
 
 int main() {
     
@@ -72,24 +78,26 @@ int randomNumber(int a, int b){
 }
 
 // this function will swap the number provided by the user
-void swapNumber(int& a, int& b){
+void swapNumber(int& a, int& b){ // int a and int b are in this function and random number function
     
-    int t = a;
+    int t = a; // here and randomnumber function
     a = b;
     b = t;
     return;
 }
 
-double PoundsToKilograms(double pounds) {
-    double totalKilos = 0;
+double PoundsToKilograms(double pounds) { // pounds is in this function and main scope
+    double totalKilos = 0; // only in this function
     
     totalKilos = pounds * (1/2.2);
     
-    return totalKilos;
+    return totalKilos; // this will return the value to where it is called
 }
 
-double KilosToPounds(double kiloGrams) {
-    double totalPounds = 0;
+// this function definition will calculate the number of pounds to kilos
+
+double KilosToPounds(double kiloGrams) { // kilograms is in this functionand main scope function
+    double totalPounds = 0; // only in this function
     
     totalPounds = kiloGrams * 2.2;
     
